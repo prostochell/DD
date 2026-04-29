249e3f77cea04e81ab8bdf7058f50a02a341a894
unknown <nazarlysenkoo@gmail.com>
2026-04-29 09:51:36 +0200

    Fixed code-style: fix mux

--- DF2_1/2/mux_2_1_a.vhd
+++ DF2_1/2/mux_2_1_a.vhd
@@ -2,15 +2,15 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity mux_2_1 is
+entity mux_2_1_a is
     port (
         A : in std_logic;
         B : in std_logic;
         SEL : in std_logic;
         Y : out std_logic);
-end mux_2_1;
+end mux_2_1_a;
 
-architecture arch_mux_2_1 of mux_2_1 is
+architecture arch_mux_2_1 of mux_2_1_a is
 begin
     with (sel) select
     Y <= A when '0',
--- DF2_1/2/mux_2_1_b.vhd
+++ DF2_1/2/mux_2_1_b.vhd
@@ -2,15 +2,15 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity mux_2_1 is
+entity mux_2_1_b is
     port (
         A : in std_logic;
         B : in std_logic;
         SEL : in std_logic;
         Y : out std_logic);
-end mux_2_1;
+end mux_2_1_b;
 
-architecture arch_mux_2_1 of mux_2_1 is
+architecture arch_mux_2_1 of mux_2_1_b is
 begin
     Y <= A when SEL = '0' else B;
-end architecture;
+end architecture;В
